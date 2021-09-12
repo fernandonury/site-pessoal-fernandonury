ba3d80b8f20237c4adf9a800a3a2b864ce850286
Fernando e Nury Biasoli <fernandonury@gmail.com>
2021-09-12 16:36:25 -0300

    commit

---
+++ script.js
@@ -0,0 +1,56 @@
+if (window.matchMedia("(max-width: 320px)").matches) {
+    
+    document.getElementById('tit00').style.marginTop = '15px'
+    document.getElementById('txt00').style.marginTop = '15px'
+
+    document.getElementById('cupido').style.display = 'none'
+
+    document.getElementById('fernandonury01').style.top = '960px'
+
+    document.getElementById('txt01').style.top = '1160px'
+
+    document.getElementById('tit01').style.top = '1770px'
+
+    document.getElementById('logo-jocum').style.top = '1787.5px'
+    document.getElementById('logo-jocum').style.marginLeft = '67%'
+
+}
+
+if (window.matchMedia("(min-width: 360px)").matches) {
+
+    document.getElementById('fernandonury01').style.top = '900px'
+
+    document.getElementById('txt01').style.top = '1130px'
+    
+    document.getElementById('tit01').style.top = '1675px'
+
+    document.getElementById('logo-jocum').style.top = '1693px'
+    document.getElementById('logo-jocum').style.marginLeft = '61%'
+
+}
+
+if (window.matchMedia("(min-width: 375px)").matches) {
+
+    document.getElementById('fernandonury01').style.top = '880px'
+
+    document.getElementById('txt01').style.top = '1120px'
+    
+    document.getElementById('tit01').style.top = '1640px'
+
+    document.getElementById('logo-jocum').style.top = '1658px'
+    document.getElementById('logo-jocum').style.marginLeft = '59%'
+
+}
+
+if (window.matchMedia("(min-width: 414px)").matches) {
+
+    document.getElementById('fernandonury01').style.top = '835px'
+
+    document.getElementById('txt01').style.top = '1110px'
+    
+    document.getElementById('tit01').style.top = '1565px'
+
+    document.getElementById('logo-jocum').style.top = '1585px'
+    document.getElementById('logo-jocum').style.marginLeft = '55%'
+
+}
